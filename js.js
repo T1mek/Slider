60e7b96eaf4735074a503cbbfca16ed5fd8d162e
Михаил <misha1234567890m@gmail.com>
2022-07-14 14:00:29 +0300

    Добавлнеие плагина

--- js.js
+++ js.js
@@ -1,15 +1,20 @@
-const slide = document.querySelectorAll('.slider')
 
-for (const slider of slide ){
-    slider.addEventListener("click",()=>{
-        Remove()
-        slider.classList.add("active")
-        
-    })
-}
+function slidePlugun(activeSlide = 1){
+    const slide = document.querySelectorAll('.slider')
 
-function Remove() {
-    slide.forEach((slider)=>{
-        slider.classList.remove("active")
-    })
+    slide[activeSlide].classList.add('active')
+    for (const slider of slide ){
+        slider.addEventListener("click",()=>{
+            Remove()
+            slider.classList.add("active")
+            
+        })
+    }
+    
+    function Remove() {
+        slide.forEach((slider)=>{
+            slider.classList.remove("active")
+        })
+    }
 }
+slidePlugun()
